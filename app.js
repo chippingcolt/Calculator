476ece5144c59229cf7074bc781674ea896a8c89
Timothy B <charliebrown1a@gmail.com>
2017-07-17 10:16:54 -0700

    Updated app.js
    Older file, does not output rgb values to the top of the game

--- app.js
+++ app.js
@@ -1,1 +1,73 @@
 
+var colors = [
+"rgb(255, 0, 0)",
+"rgb(255, 255, 0)",
+"rgb(0, 255, 0)",
+"rgb(0, 255, 255)",
+"rgb(0, 0, 255)",
+"rgb(255, 0, 255)",
+]
+var colors = generateRandomColors(6);
+
+var squares = document.querySelectorAll(".square");
+var pickedColor = pickColor();
+var colorDisplay = document.getElementById("colorDisplay");
+var messageDisplay = document.querySelector("#message");
+
+
+colorDisplay.textContent = pickedColor;
+
+for(var i = 0; i < squares.length; i++){
+	//add initial colors to squares
+	squares[i].style.background = colors[i];
+
+
+	//add click listeners to squares
+	squares[i].addEventListener("click", function(){
+		//grab color of clicked squares
+		var clickedColor= this.style.background;
+		//compare color to pickedColor
+		if(clickedColor === pickedColor){
+			messageDisplay.textContent = "Correct!";
+			changeColors(clickedColor);
+		} else {
+			this.style.background = "#232323";
+			messageDisplay.textContent = "Try Again";
+		}
+	});
+}
+
+function changeColors(color){
+	//loop through all squares
+	for(var i = 0; i < squares.length; i++){
+
+	squares[i].style.background = color;
+
+	}
+}
+
+
+function pickColor(){
+	var random = Math.floor(Math.random() * colors.length);
+	return colors[random];
+}
+
+function generateRandomColors(num){
+	//make an array
+	var arr = [];
+	//add num random colors to array
+	for(var i = 0; i < num; i++){
+
+	}
+	//return that array
+	return arr;
+}
+
+function randomColor(){
+	//pick a "red" from 0-255
+	Math.floor(Math.random()*256)
+	//pick a "green" from 0-255
+	Math.floor(Math.random()*256)
+	//pick a "blue" from 0-255
+	Math.floor(Math.random()*256)
+}
